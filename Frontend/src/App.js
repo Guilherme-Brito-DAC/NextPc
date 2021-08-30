0dd6675ce74e65134fc2d455b9b4eca6d66a8863
Arthur Araújo <arthuraraujo07@gmail.com>
2021-08-29 21:44:43 -0300

    Inicio da página home

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div class="header">
+        <Button variant="contained">Home</Button>
+        <Button variant="contained">Meu PC</Button>
+        <Button variant="contained">Procurar Peças</Button>
+        <Button variant="contained">Contato</Button>
+        <Button variant="contained">Sobre</Button>
+        <div class="account_button">
+          <Button>
+            Minha Conta
+          </Button>
+          <Menu>
+            <MenuItem onClick="">Editar</MenuItem>
+            <MenuItem onClick="">Sair</MenuItem>
+          </Menu>
+        </div>
 
+
+
+
+
+      </div>
+    </div>
+  )
+}
 export default App;
