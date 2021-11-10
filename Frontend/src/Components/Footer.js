4a538e7418dcc5464f118830c131ebc25d5906e3
Guilherme Brito de Almeida Costa <guilhermebritoalmeidacosta@gmail.com>
2021-11-10 18:18:08 -0300

    Footer

--- Frontend/src/Components/Footer.js
+++ Frontend/src/Components/Footer.js
@@ -2,14 +2,25 @@ import React from 'react'
 import './Footer.css'
 
 function Footer() {
-    return(
-        <> 
+    return (
+        <>
             <footer>
-                Oi
+                <a href="mailto:nextgearprofissional@gmail.com?Subject=Me%20ajude" target="_blank">
+                    <img src="https://img.icons8.com/color/40/000000/gmail-new.png" alt=""/>
+                </a>
+                <a href="https://www.facebook.com/Next-Gear-178279354272838/?ref=page_internal" target="_blank">
+                    <img src="https://img.icons8.com/color/40/000000/facebook-new.png" alt=""/>
+                </a>
+                <a href="https://www.instagram.com/nextgearprofissional/" target="_blank">
+                    <img src="https://img.icons8.com/fluency/40/000000/instagram-new.png"/>
+                </a>
+                <a href="" target="_blank">
+                    <img src="https://img.icons8.com/color/40/000000/twitter--v1.png" alt=""/>
+                </a>
             </footer>
         </>
     )
-    
+
 }
 
 export default Footer
