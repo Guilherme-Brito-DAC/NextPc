d99940905312d5521e6f3d34e68b18a98f18bcc2
Arthur Araújo <74935214+Tuzin1232@users.noreply.github.com>
2021-12-06 17:34:37 -0300

    Formulário de alteração de conta feito (falta fazer um alerta para efeitos visuais)

--- Frontend/src/Pages/Conta.js
+++ Frontend/src/Pages/Conta.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react/cjs/react.development'
 import Swal from 'sweetalert2'
 
 function Conta({ Usuario, SetUsuario, Token, Deslogar }) {
-    
+
     const [UsuarioForm, SetUsuarioForm] = useState({
         "nome": '',
         "sobrenome": '',
@@ -20,11 +20,11 @@ function Conta({ Usuario, SetUsuario, Token, Deslogar }) {
     })
 
     const h = useHistory()
-    useEffect(()=>{
+    useEffect(() => {
         if (!Usuario || !Token) {
             h.push("/pecas/cpu/1")
         }
-        else{
+        else {
             SetUsuarioForm({
                 "nome": Usuario.nome,
                 "sobrenome": Usuario.sobrenome,
@@ -39,8 +39,8 @@ function Conta({ Usuario, SetUsuario, Token, Deslogar }) {
                 "modoNoturno": Usuario.modoNoturno
             })
         }
-    },[Usuario,Token])
-    
+    }, [Usuario, Token])
+
     function HandleSubmit(e) {
         e.preventDefault();
 
@@ -67,7 +67,7 @@ function Conta({ Usuario, SetUsuario, Token, Deslogar }) {
             showCancelButton: true,
             confirmButtonText: 'Sim',
             denyButtonText: `Não`,
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
                 fetch('https://localhost:5001/api/usuario', {
                     method: 'DELETE',
@@ -82,22 +82,42 @@ function Conta({ Usuario, SetUsuario, Token, Deslogar }) {
                         Deslogar()
                     })
             } else if (result.isDenied) {
-              
+
             }
-          })
+        })
     }
 
 
     return (
         <>
+
             <form className="background" onSubmit={HandleSubmit}>
-
-                {/*Exemplo de formulário */}
-                <input type="text" className="input" value={UsuarioForm.nome} onChange={(e) => { SetUsuarioForm({ ...UsuarioForm, nome: e.target.value }) }} placeholder="Nome" aria-label="Nome" />
-
+                <div className="form">
+                    <div className="row">
+                        <div className="col">
+                            <input type="text" className="input" value={UsuarioForm.nome} onChange={(e) => { SetUsuarioForm({ ...UsuarioForm, nome: e.target.value }) }} placeholder="Nome" aria-label="Nome" required />
+                        </div>
+                        <div className="col">
+                            <input type="text" className="input" value={UsuarioForm.sobrenome} onChange={(e) => { SetUsuarioForm({ ...UsuarioForm, sobrenome: e.target.value }) }} placeholder="Sobrenome" aria-label="Sobrenome"  />
+                        </div>
+                    </div>
+                    <br />
+                    <div className="col">
+                        <input type="text" className="input" value={UsuarioForm.usuario} onChange={(e) => { SetUsuarioForm({ ...UsuarioForm, usuario: e.target.value }) }} placeholder="Usuario" required />
+                        <br />
+                        <input type="email" className="input" value={UsuarioForm.email} onChange={(e) => { SetUsuarioForm({ ...UsuarioForm, email: e.target.value }) }} placeholder="Email" required />
+                        <br />
+                        <input type="password" className="input" value={UsuarioForm.senha} onChange={(e) => { SetUsuarioForm({ ...UsuarioForm, senha: e.target.value }) }} placeholder="Senha" required />
+                        <br />
+                    </div>
+                    <br />
+                    <button type="button" onClick={HandleSubmit} className="btn btn-success">Editar Informações</button>
+                </div>
+                <br />
             </form>
+            <br />
             <form className="background" onSubmit={Deletar}>
-                <button type="submit">Deletar</button>
+                <button className="btn btn-warning" type="submit">Deletar</button>
             </form>
         </>
     )
